713cfb482d3f8f08edef5ddb41afd324a3ee8b99
Andres Tombo <117206225+RegahP@users.noreply.github.com>
2023-10-11 21:43:54 -0300

    Add files via upload

---
+++ xerrainfo.cpp
@@ -0,0 +1,227 @@
+xerra arc structure {
+    essentia en busca de mira {
+        leo + mira duerme {
+			
+        }
+        leo + mira despierta + rebels en la tierra {
+			incidente en el chino {
+			
+			}
+			incidente en la huerta de mama {
+			
+			}
+			incidente droga de sylvia {
+			
+			}
+			++
+		}
+		++
+		the gang viajan a xerra {
+			yuii pom pom concert {
+			
+			}
+		}
+    }
+}
+
+xerra taxonomy {
+	biology { //nature of xerran life
+		archaxia {
+			//contains enerxy
+			primia {
+				//fundamental entity
+				concordia {
+					//universe cycle
+					sententia {
+						//meaning
+						virtua {
+							//strength
+							terra
+							vitreus
+							aes
+						}
+						halcyon {
+							//temperance
+							ignis
+							helia
+							diruo
+						}
+					}
+					animantia {
+						//conscience
+						alasen {
+							//life
+							aeras
+							fluxum
+							dendro
+						}
+						procyon {
+							//death
+							exera
+							eruptis
+							inaxis
+						}
+					}
+					omnia {
+						//everything
+						essentia //omnian plane
+						ethernia //aevus plane
+					}
+				}
+			}
+			aprimia {
+				//non fundamental entity
+				cruxarea {
+					//with headspace
+					xerrano / xerranos
+				}
+				acruxarea {
+					//without headspace
+					erradia / erradias //nomad creatures
+					hybria / hybrias //hybrid creatures
+					rexesia / rexesias //sedentary creatures
+				}
+			}
+
+		}
+
+		nihilixia {
+			//rejects enerxy
+			nil {
+				absentia
+			}
+		}
+	}
+
+	thaumology { //magic system
+		archimia {
+			//practice of equivalent exoergic transformation
+			suixia //self-composition
+			creana //matter mutation
+		}
+
+		ressuo {
+			//magic that connects dimensions
+			omnia //fourth-dimensional plane
+			aevus //fifth-dimensional plane
+		}
+	} 
+
+	skills { //ars-elemental ennumerated techniques 
+		//unuo, duo, trio, quadria, pentia, sexia, septea, octo, nova, dexea
+		terra {
+			behemoth //earthquake
+		}
+		vitreus {
+			glass man //crystal prison
+			lightbringer //ethereal blinding godrays
+		}
+		aes {
+			cyber grind //steel pillars crush
+		}
+		ignis {
+			black hole sun //solar core
+		}
+		helia {
+			extasis en estasis //aoe burst freeze
+		}
+		diruo {
+			vertigo //aoe gravitational impact
+			singing void //spawns black hole, kills it
+		}
+		aeras {
+			sky fracture //fractures air density into fragments, change sky colors, move clouds
+		}
+		fluxum {
+			hydrogen //high pressure piercing water shot
+		}
+		dendro {
+			jungle fury //aoe vegetation overgrowth
+		}
+		ardenas {
+			
+		}
+		exera {
+			poison turtle
+		}
+		morbo {
+			parasite eve
+			witness the death //ego-death experience
+			death kiss infinity
+		}
+		omnia {
+			pierce the veil //perforation of the omnia plane
+			goodbye to a world //planetary dissintegration
+		}
+		aevus {
+			moment
+			youthanasia
+		}
+	} 
+
+	creana spectrums {
+		terra {
+			crafting, stone, dirt, concrete, rocks, mountains,
+		}
+		vitreus {
+			crafting, sand, crystal shards, mirrors, prisms, optics,
+		}
+		aes {
+			crafting, machinery, mechanics, weapons, tools,
+		}
+		ignis {
+			heat, pressure, orange fire, blue fire, aurora, lightning, solar matter
+		}
+		helia {
+			cold, pressure, vacuum, freeze, frostbite, parastasis,
+		}
+		diruo {
+			gravity magnitude & direction, levitation, aoe, compression, black holes
+		}
+		aeras {
+			wind, flight, clouds, sky properties, storm, hurricane, tornado
+		}
+		fluxum {
+			humidity, vapor, mist, waves, rain, watershot, tsunami, 
+		}
+		dendro {
+			peace, awareness, growth, enlightment, nourishment, healing, life
+		}
+		ardenas {
+			light phenomena, ultraviolet, xrays, gamma radiation, carcinogens
+		}
+		exera {
+			erosion, acid, rot, corrosion, dissolution, decomposition
+		}
+		morbo {
+			pain, torment, depression, fear, sickness, disease, decay, death
+		}
+		omnia {
+			sound waves, light rays, distortions, wormholes, super black holes, mass destruction
+		}
+		aevus {
+			time dilation, time stretch, time freeze, time reverse, time burst, aoe
+		}
+	}
+
+	archemist classes { //dnd style classes
+		warean / warrior { //weapon based
+			claxier / clasher //melee fighter
+			ranxer / ranger //ranged fighter
+		}
+		efferean / invoker { //cast based
+			accrexier / augmenter // melee fighter
+			hexiser / sorcerer // ranged fighter
+		}
+		sphexscier / specialist { //tools & augmenting
+			opiphexer / technician //enerxy mechanic
+			artiphexer / craftsman //artisan builder
+		}
+		cruxscier / corelogian { //those who study the core
+			xahman / naturist //shamanic medic
+			suiscier / spiritualist //spiritual guru
+			alternia / shapeshifter //body morpher
+		}
+	}
+}
+
